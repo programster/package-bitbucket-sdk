e6437219559e214384a7cdc049b438f1ba7a13f8
Programster <sdpagent@gmail.com>
2022-09-22 15:31:39 +0100

    Changes DeploymentEnvironment to have a private constructor, with another static method of createNew with less attributes, for users to use when wishing to create a new environment.

--- src/models/DeploymentEnvironment.php
+++ src/models/DeploymentEnvironment.php
@@ -3,62 +3,88 @@
 namespace Programster\Bitbucket\Models;
 
 
-class DeploymentEnvironment implements \JsonSerializable
+final class DeploymentEnvironment implements \JsonSerializable
 {
     private string $m_uuid;
     private string $m_type;
     private string $m_name;
     private string $m_slug;
-    private int    $m_rank;
     private EnvironmentType $m_environmentType;
     private bool $m_hidden;
-    private bool $m_environmentLockEnabled;
-    private bool $m_deploymentGateEnabled;
     private DeploymentEnvironmentCategory $m_category;
-    private DeploymentEnvironmentLock $m_lock;
+    private ?int   $m_rank;
+    private ?bool $m_deploymentGateEnabled;
+    private ?bool $m_environmentLockEnabled;
+    private ?DeploymentEnvironmentLock $m_lock;
 
 
-    public function __construct(
-        string $uuid,
-        string $name,
-        string $slug,
-        int $rank,
-        EnvironmentType $environmentType,
-        DeploymentEnvironmentCategory $category,
-        DeploymentEnvironmentLock $lock,
-        bool $environmentLockEnabled,
-        bool $m_deploymentGateEnabled = false,
-        bool $hidden = false,
-    )
+    private function __construct()
     {
-        $this->m_uuid = $uuid;
-        $this->m_name = $name;
-        $this->m_slug = $slug;
-        $this->m_rank = $rank;
-        $this->m_environmentType = $environmentType;
-        $this->m_hidden = $hidden;
-        $this->m_environmentLockEnabled = $environmentLockEnabled;
-        $this->m_deploymentGateEnabled = $m_deploymentGateEnabled;
-        $this->m_category = $category;
+        // this always has to be this value and not to be confused with EnvironmentType
+        $this->m_type = "deployment_environment";
     }
 
 
-    public static function createFromResponseArray(array $responseData)
+    /**
+     * Create a new environment, for POSTing to Bitbucket.
+     * @param string $uuid
+     * @param string $name
+     * @param string $slug
+     * @param EnvironmentType $environmentType
+     * @param string $categoryName
+     * @param bool $hidden
+     * @param int|null $rank
+     * @return DeploymentEnvironment
+     */
+    public static function createNew(
+        string $uuid,
+        string $name,
+        string $slug,
+        EnvironmentType $environmentType,
+        string $categoryName,
+        bool $hidden = false,
+        ?int $rank = null,
+    ) : DeploymentEnvironment
     {
-        $deploymentEnvironment = new DeploymentEnvironment(
-            uuid: $responseData['uuid'],
-            name: $responseData['name'],
-            slug: $responseData['slug'],
-            rank: $responseData['rank'],
-            environmentType: EnvironmentType::createFromResponseArray($responseData['environment_type']),
-            category: new DeploymentEnvironmentCategory($responseData['category']['name']),
-            lock: new DeploymentEnvironmentLock(
-                $responseData['lock']['type'],
-                $responseData['lock']['name'],
-            ),
-            environmentLockEnabled: $responseData['environment_lock_enabled'],
-            hidden: $responseData['hidden'],
+        $deploymentEnvironment = new DeploymentEnvironment();
+        $deploymentEnvironment->m_uuid = $uuid;
+        $deploymentEnvironment->m_name = $name;
+        $deploymentEnvironment->m_slug = $slug;
+        $deploymentEnvironment->m_category = new DeploymentEnvironmentCategory($categoryName);
+        $deploymentEnvironment->m_environmentType = $environmentType;
+        $deploymentEnvironment->m_hidden = $hidden;
+
+        $deploymentEnvironment->m_rank = $rank;
+        $deploymentEnvironment->m_lock = null;
+        $deploymentEnvironment->m_environmentLockEnabled = null;
+        return $deploymentEnvironment;
+    }
+
+
+    /**
+     * Create an environment from a Bitbucket response array.
+     * @param array $responseData
+     * @return DeploymentEnvironment
+     * @throws \Programster\Bitbucket\Exceptions\ExceptionMissingRequiredKeys
+     */
+    public static function createFromResponseArray(array $responseData) : DeploymentEnvironment
+    {
+        $deploymentEnvironment = new DeploymentEnvironment();
+        $deploymentEnvironment->m_uuid = $responseData['uuid'];
+        $deploymentEnvironment->m_name = $responseData['name'];
+        $deploymentEnvironment->m_slug = $responseData['slug'];
+        $deploymentEnvironment->m_rank = $responseData['rank'];
+        $deploymentEnvironment->m_environmentType = EnvironmentType::createFromResponseArray($responseData['environment_type']);
+
+        $deploymentEnvironment->m_category = new DeploymentEnvironmentCategory($responseData['category']['name']);
+
+        $deploymentEnvironment->m_lock = new DeploymentEnvironmentLock(
+            $responseData['lock']['type'],
+            $responseData['lock']['name'],
         );
+
+        $deploymentEnvironment->m_environmentLockEnabled = $responseData['environment_lock_enabled'];
+        $deploymentEnvironment->m_hidden = $responseData['hidden'];
 
         return $deploymentEnvironment;
     }
@@ -66,19 +92,22 @@ class DeploymentEnvironment implements \JsonSerializable
 
     public function toArray() : array
     {
-        return [
+        $arrayForm = [
             'uuid' => $this->m_uuid,
             'type' => $this->m_type, // should always be 'deployment_environment',
             'name' => $this->m_name,
             'slug' => $this->m_slug,
-            'rank' => $this->m_rank,
             'hidden' => $this->m_hidden,
             'environment_type' => $this->m_environmentType->toArray(),
-            'environment_lock_enabled' => $this->m_environmentLockEnabled,
-            'deployment_gate_enabled' => $this->m_deploymentGateEnabled,
             'category' => $this->m_category,
-            'lock' => $this->m_lock,
         ];
+
+        if ($this->m_rank !== null) { $arrayForm['rank'] = $this->m_rank; }
+        if ($this->m_environmentLockEnabled !== null) { $arrayForm['environment_lock_enabled'] = $this->m_environmentLockEnabled; }
+        if ($this->m_deploymentGateEnabled !== null) { $arrayForm['deployment_gate_enabled'] = $this->m_deploymentGateEnabled; }
+        if ($this->m_lock !== null) { $arrayForm['lock'] = $this->m_lock; }
+
+        return $arrayForm;
     }
 
 
