0892a56aef57d12fa6121a6d4279de4bed6aa953
Programster <sdpagent@gmail.com>
2022-09-22 12:04:34 +0100

    fixes issue with custom exception

--- src/exceptions/ExceptionMissingRequiredKeys.php
+++ src/exceptions/ExceptionMissingRequiredKeys.php
@@ -7,7 +7,7 @@ class ExceptionMissingRequiredKeys extends \Exception
 {
     public function __construct(private readonly array $keys, ?string $message = null)
     {
-        $message = $message ?? "Missing required keys: " .  implode(", " . $keys);
+        $message = $message ?? "Missing required keys: " .  implode(", ", $keys);
         parent::__construct($message);
     }
 
