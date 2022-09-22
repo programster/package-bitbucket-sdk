8377fa81883318a460423a207a32eef03a1a91db
Programster <sdpagent@gmail.com>
2022-09-22 16:07:15 +0100

    updates DeploymentEnvironmentCategory to be JSON serializable.

--- src/models/DeploymentEnvironmentCategory.php
+++ src/models/DeploymentEnvironmentCategory.php
@@ -2,12 +2,28 @@
 
 namespace Programster\Bitbucket\Models;
 
-class DeploymentEnvironmentCategory
+class DeploymentEnvironmentCategory implements \JsonSerializable
 {
     public function __construct(private readonly string $name)
     {
 
     }
 
+
+    public function toArray() : array
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
+    }
+
+
+    # Accessors
     public function getName() : string { return $this->name; }
 }
